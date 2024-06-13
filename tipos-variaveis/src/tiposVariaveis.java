72980bebe7f55b12ccfb86782ec9f80b56780221
Pedro Janeo <pdro.henriquejanneo@gmail.com>
2024-06-13 12:48:15 -0300

    testenado o constante

--- tipos-variaveis/src/tiposVariaveis.java
+++ tipos-variaveis/src/tiposVariaveis.java
@@ -2,7 +2,10 @@ public class tiposVariaveis {
     public static void main(String[] args) throws Exception {
        
 
-        double salarioNinimo = 2500;
+        double salarioNinimo = 2500; // variavel ( muda de valor )
         System.out.println(salarioNinimo);
+
+        final double minimo = 10; // o 'final' faz a variavel virar constante ( constante nao muda de valor)
+        System.out.println(minimo);
     }
 }
