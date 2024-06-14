372b30d6fdd9f6d6717f9367e7de6440ded80a7a
Pedro Janeo <pdro.henriquejanneo@gmail.com>
2024-06-14 13:50:02 -0300

    operador ternario

--- tipos-variaveis/src/Operaores.java
+++ tipos-variaveis/src/Operaores.java
@@ -7,12 +7,21 @@ public class Operaores {
        System.out.println( n == 5);
 
        n = 5;
-       n += 2;
-       System.out.println(n);
+    //    n += 2;
+    //    System.out.println(n);
 
-       n -= 2;
-       System.out.println(n);
+    //    n -= 2;
+    //    System.out.println(n);
 
-        
+       int m = 4;
+
+        if ( n == m){
+            System.out.println("V");
+        } else {
+            System.out.println("F");
+        }
+
+        String resultado = n==m ? "verdadeiro" : "falso";
+        System.out.println(resultado);
     }
 }
