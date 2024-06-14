9a5ec9c24242165fdf08d8c7f42e66f1230da16a
Pedro Janeo <pdro.henriquejanneo@gmail.com>
2024-06-14 13:39:47 -0300

    operadores

--- tipos-variaveis/src/Operaores.java
+++ tipos-variaveis/src/Operaores.java
@@ -1,12 +1,14 @@
 public class Operaores {
     public static void main(String[] args) {
         
-        System.out.println("teste");
 
         int n = 5;
 
        System.out.println( n == 5);
 
+       n = 5;
+       n += 2;
+       System.out.println(n);
 
 
     }
