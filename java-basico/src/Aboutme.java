ebfb7ef7c8293a717d1e71ca601a1b47d521b237
Pedro Janeo <pdro.henriquejanneo@gmail.com>
2024-06-17 12:46:36 -0300

    scanner

--- java-basico/src/Aboutme.java
+++ java-basico/src/Aboutme.java
@@ -1,15 +1,25 @@
+import java.util.Scanner;
+
 public class Aboutme {
     public static void main(String[] args) {
         
-        String nome = args [0];
-        String sobrenome = args [1];
-        int idade = Integer.valueOf(args [2]);
-        double altura = Double.valueOf(args[3]);
+       Scanner sc = new Scanner(System.in);
 
-        System.out.println("Ola me chamo " + nome + " " + sobrenome);
-        System.out.println("Tenhp " + idade + " anos ");
-        System.out.println("Minha altura é " + altura + " cm" );
+        System.out.println("Digite seu nome ");
+        String nome = sc.nextLine();
 
-        //execuções feitas pelo terminal!
+        System.out.println("Digite sobrenome: ");
+        String sobrenome = sc.nextLine();
+
+        System.out.println("Digite sua idade ");
+        int idade = sc.nextInt();
+
+        System.out.println("Digite sua altura ");
+        double altura = sc.nextDouble();
+
+        System.out.println("Me chamo " + nome + " " +sobrenome + " tenho " + idade + " e " + altura + "cm");
+
+
+       sc.close();
     }
 }
