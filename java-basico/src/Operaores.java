7e8ba531a0b4bb83ca24b497b5123518b5efc728
Pedro Janeo <pdro.henriquejanneo@gmail.com>
2024-06-17 09:47:33 -0300

    renomeia

---
+++ java-basico/src/Operaores.java
@@ -0,0 +1,31 @@
+public class Operaores {
+    public static void main(String[] args) {
+        
+
+        int n = 5;
+
+       System.out.println( n == 5);
+
+       n = 5;
+    //    n += 2;
+    //    System.out.println(n);
+
+    //    n -= 2;
+    //    System.out.println(n);
+
+       int m = 4;
+
+        if ( n == m){
+            System.out.println("V");
+        } else {
+            System.out.println("F");
+        }
+
+        String resultado = n==m ? "verdadeiro" : "falso"; // operador ternario (forma resumida do if/else)
+        System.out.println(resultado);                      //primeira condicao V/if, segunda F/else
+
+        String nomeUm = "JANNEO";
+        String nomeDois = new String("JANNEO");  // new = novo objeto
+        System.out.println(nomeUm.equals(nomeDois)); // .equals compara CONTEUDO diferente do == que compara valor
+    }
+}
---
+++ java-basico/src/TiposVariaveis.java
@@ -0,0 +1,12 @@
+public class TiposVariaveis {
+    public static void main(String[] args) throws Exception {
+       
+
+        double salarioNinimo = 2500; // variavel ( muda de valor )
+        System.out.println(salarioNinimo);
+
+        final double minimo = 10; // o 'final' faz a variavel virar constante ( constante nao muda de valor)
+        System.out.println(minimo); // esse metodo 'final' é usado mto em valor que nao muda, ex: no PI
+        System.out.println("Hello world");
+    }
+}
