aa6a8c9925c6f0392750569a38f96d2c5a8df6f6
Pedro Janeo <pdro.henriquejanneo@gmail.com>
2024-06-21 08:31:18 -0300

    Getter e Setter

---
+++ POO/src/escola/Escola.java
@@ -0,0 +1,14 @@
+package escola;
+
+public class Escola {
+    public static void main(String[] args) {
+        
+    
+    Aluno felipe = new Aluno();
+		felipe.setNome("Peu");
+		felipe.setIdade(19);
+		
+		System.out.println("O aluno " + felipe.getNome() + " tem " + felipe.getIdade() + " anos ");
+		
+}
+}
