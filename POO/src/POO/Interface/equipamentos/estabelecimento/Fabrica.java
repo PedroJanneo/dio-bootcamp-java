0babfb8d934b37412e05ebd3c74cfed393f104d6
Pedro Janeo <pdro.henriquejanneo@gmail.com>
2024-06-24 13:09:58 -0300

    INTERFACE

--- POO/src/POO/Interface/equipamentos/estabelecimento/Fabrica.java
+++ POO/src/POO/Interface/equipamentos/estabelecimento/Fabrica.java
@@ -1,12 +1,22 @@
 package POO.Interface.equipamentos.estabelecimento;
 
-import POO.Interface.equipamentos.impressora.Laserjet;
+// import POO.Interface.equipamentos.impressora.Laserjet;
+import POO.Interface.equipamentos.multi.EquipamentoMultifuncional;
+import POO.Interface.equipamentos.Digitalizadora.Digitalizadora;
+import POO.Interface.equipamentos.copiadora.Copiadora;
 import POO.Interface.equipamentos.impressora.Impressora;
 
 
 public class Fabrica {
     public static void main(String[] args) {
-        Impressora impressora = new Laserjet();
+
+        EquipamentoMultifuncional em = new EquipamentoMultifuncional();
+        Impressora impressora = em;
+        Digitalizadora digitalizadora = em;
+        Copiadora copiadora = em;
+        
         impressora.imprimir();
+        digitalizadora.digitalizar();;
+        copiadora.copiar();
     }
 }
